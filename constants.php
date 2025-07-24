a4da60112df75b8d529c1d39e50de65a729f8c1b
vignesh-gitlab <113657987+vignesh-gitlab@users.noreply.github.com>
2025-07-24 10:03:49 +0530

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,6 @@
+<?php
+define('STRIPE_API_SECRET_KEY', 'sk_test_51MSANWSIlGUfiWTIsoLgEreUNdxYJQqR8A3EX0kKpjCV67MKH0YzyEqZGSkGfbhgwK3VfOF5FH0jYWjbIvmZBngu007uSbCee6');
+define('STRIPE_API_PUBLISH_KEY', 'pk_test_51MSANWSIlGUfiWTI1AKW1BiusMGr6yNFFm12vfL0dpI4jx2GCJ7xk7wGctYPicefpIin9rWVOy4ja1d6gDBfil7u00wylTjO4s');
+
+
+//
